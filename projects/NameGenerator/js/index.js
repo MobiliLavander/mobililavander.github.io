9203728bb7d55447fd1564c4d63f0b5348c3a2a8
MobiliLavander <ericdilenardo91@gmail.com>
2022-02-13 18:24:43 +0100

    Update index.js

--- projects/NameGenerator/js/index.js
+++ projects/NameGenerator/js/index.js
@@ -1,7 +1,7 @@
 function singleExtraction() {
     let nomiEstratti = [];
     const nomiDaEstrarre = 4;
-    const nomiEstraibili = ["Basa", "Bhoyrub", "Boezio", "Cossettini", "Cova", "D'Agosto", "Di Lenardo", "Dordolo", "Ellero", "Fabbro", "Garofolo", "Giancristofaro", "Moro", "Nobile", "Pagnutti", "Pellegrini", "Pellizzari", "Ruffo", "Savorgano", "Venutrini"];
+    const nomiEstraibili = ["Basa", "Bhoyrub", "Boezio", "Cossettini", "Cova", "D'Agosto", "Di Lenardo", "Dordolo", "Ellero", "Fabbro", "Garofolo", "Giancristofaro", "Iacuzzi", "Moro", "Nobile", "Pagnutti", "Pellegrini", "Pellizzari", "Ruffo", "Savorgano", "Venutrini"];
 
     for (let i = 0; i = nomiDaEstrarre - nomiEstratti.length; i++) {
         let nomeCorrente = nomiEstraibili[(Math.floor(Math.random() * nomiEstraibili.length))]
