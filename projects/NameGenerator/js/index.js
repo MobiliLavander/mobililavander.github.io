ae9befe2c16fd7b953421ad3e60dd07df80683ae
MobiliLavander <ericdilenardo91@gmail.com>
2022-02-19 19:06:43 +0100

    Update

--- projects/NameGenerator/js/index.js
+++ projects/NameGenerator/js/index.js
@@ -1,18 +1,21 @@
 function singleExtraction(nomi, gruppi) {
+    let gruppiEstratti = [];
     let nomiEstratti = [];
     const nomiDaEstrarre = 4;
     const nomiEstraibili = ["Basa", "Bhoyrub", "Boezio", "Cossettini", "Cova", "D'Agosto", "Di Lenardo", "Dordolo", "Ellero", "Fabbro", "Garofolo", "Giancristofaro", "Iacuzzi", "Moro", "Nobile", "Pagnutti", "Pellegrini", "Pellizzari", "Ruffo", "Savorgano", "Venutrini"];
 
-    for (let i = 0; i = nomi - nomiEstratti.length; i++) {
-        let nomeCorrente = nomiEstraibili[(Math.floor(Math.random() * nomiEstraibili.length))]
-        if (nomiEstratti.includes(nomeCorrente)) {
+    for (let i = 0; i = gruppi - gruppiEstratti.lenght; i++) {
+        for (let i = 0; i = nomi - nomiEstratti.length; i++) {
+            let nomeCorrente = nomiEstraibili[(Math.floor(Math.random() * nomiEstraibili.length))]
+            if (nomiEstratti.includes(nomeCorrente)) {
 
-        } else {
-            nomiEstratti.push(nomeCorrente);
+            } else {
+                nomiEstratti.push(nomeCorrente);
+            }
         }
+        nomiEstratti.toString();
+        console.log(nomiEstratti.join(" "));
+        document.getElementById("Result").innerHTML = `${nomiEstratti.join(" ")}`;
+        nomiEstratti = [];
     }
-    nomiEstratti.toString();
-    console.log(nomiEstratti.join(" "));
-    document.getElementById("Result").innerHTML = `${nomiEstratti.join(" ")}`;
-    nomiEstratti = [];
 };
