2edf1702061f0dcb9b0975eda24e88f7ff64236f
MobiliLavander <ericdilenardo91@gmail.com>
2023-12-28 19:18:15 +0100

    Update index.js

--- projects/BezierCurves/js/index.js
+++ projects/BezierCurves/js/index.js
@@ -5,5 +5,25 @@ function setup() {
 function draw() {
     background(56, 53, 53)
     noFill();
-    bezier(85, 20, 0, 0, 0, 0, 15, 80);
+    let x1 = 85,
+        x2 = 10,
+        x3 = 90,
+        x4 = 15;
+    let y1 = 20,
+        y2 = 10,
+        y3 = 90,
+        y4 = 80;
+    bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+    fill(255);
+    let steps = 10;
+    for (let i = 0; i <= steps; i++) {
+        let t = i / steps;
+        let x = bezierPoint(x1, x2, x3, x4, t);
+        let y = bezierPoint(y1, y2, y3, y4, t);
+        circle(x, y, 5);
+    }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
